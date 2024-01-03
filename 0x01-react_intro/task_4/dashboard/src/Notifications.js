1b952af33ec93a178456808a4ae077139b2a9a4c
Menna Mohamed <79084467+MennaAnwar@users.noreply.github.com>
2024-01-03 22:29:07 +0200

    deploy

---
+++ 0x01-react_intro/task_4/dashboard/src/Notifications.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "./Notifications.css";
+import closeIcon from "./close-icon.png";
+import { getLatestNotification } from "./utils";
+
+const Notifications = () => {
+  return (
+    <div className="Notifications">
+      <div className="notify">
+        <p>Here is the list of notifications</p>
+        <button
+          className="close"
+          aria-label="Close"
+          onClick={console.log("Close button has been clicked")}
+        >
+          <img src={closeIcon} alt="Close" />
+        </button>
+      </div>
+      <ul>
+        <li data="default">New course available</li>
+        <li data="urgent">New resume available</li>
+        <li
+          data="urgent"
+          dangerouslySetInnerHTML={{ __html: getLatestNotification() }}
+        ></li>
+      </ul>
+    </div>
+  );
+};
+
+export default Notifications;
