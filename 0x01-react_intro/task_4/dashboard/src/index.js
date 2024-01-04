ba13163906f27e1bef1b73daff465738f99a102d
Menna Mohamed <79084467+MennaAnwar@users.noreply.github.com>
2024-01-04 14:08:36 +0200

    .

--- 0x01-react_intro/task_4/dashboard/src/index.js
+++ 0x01-react_intro/task_4/dashboard/src/index.js
@@ -5,23 +5,17 @@ import reportWebVitals from "./reportWebVitals";
 import Notifications from "./Notifications";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-const notifications = ReactDOM.createRoot(
-  document.getElementById("root-notifications")
-);
-
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
 
-notifications.render(
+const rootNotifications = ReactDOM.createRoot(document.getElementById("root-notifications"));
+rootNotifications.render(
   <React.StrictMode>
     <Notifications />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
