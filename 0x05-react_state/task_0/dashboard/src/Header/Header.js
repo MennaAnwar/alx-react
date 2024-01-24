755ba17ac3e89fd9bd34765f87b81ef324e16763
Menna Mohamed <79084467+MennaAnwar@users.noreply.github.com>
2024-01-24 12:51:07 +0200

    'task0'

--- 0x05-react_state/task_0/dashboard/src/Header/Header.js
+++ 0x05-react_state/task_0/dashboard/src/Header/Header.js
@@ -1,14 +1,31 @@
-import React from 'react';
-import logo from '../assets/holberton_logo.jpg';
-import './Header.css';
+import React from "react";
+import holberton_logo from "../assets/holberton_logo.jpg";
+import { StyleSheet, css } from "aphrodite";
 
 function Header() {
   return (
-    <div className="App-header">
-      <img src={logo} alt="logo" />
+    <div className={css(styles.header)}>
+      <img src={holberton_logo} className={css(styles.headerImg)} />
       <h1>School dashboard</h1>
     </div>
-  )
+  );
 }
+
+const cssVars = {
+  mainColor: "#e01d3f",
+};
+
+const styles = StyleSheet.create({
+  header: {
+    display: "flex",
+    alignItems: "center",
+    color: cssVars.mainColor,
+    fontSize: "20px",
+  },
+
+  headerImg: {
+    width: "200px",
+  },
+});
 
 export default Header;
